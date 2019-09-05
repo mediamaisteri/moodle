8e7ca7a94488f515af8767a0513f6d018d0184be
Mikko Haikonen <mikko.haikonen@mediamaisteri.com>
2019-09-05 11:53:29 +0300

    Adds password expiration for email-based authentication

--- auth/email/settings.php
+++ auth/email/settings.php
@@ -39,6 +39,47 @@ if ($ADMIN->fulltree) {
         new lang_string('auth_emailrecaptcha_key', 'auth_email'),
         new lang_string('auth_emailrecaptcha', 'auth_email'), 0, $options));
 
+    // Password expiration options.
+    $expirationoptions = array(
+        new lang_string('no'),
+        new lang_string('yes'),
+    );
+
+    $settings->add(new admin_setting_configselect('auth_email/expiration',
+        new lang_string('expiration', 'auth_email'),
+        new lang_string('expiration_desc', 'auth_email'), 0, $expirationoptions));
+
+    $expirationtimeoptions = array(
+        '30' => new lang_string('numdays', '', 30),
+        '60' => new lang_string('numdays', '', 60),
+        '90' => new lang_string('numdays', '', 90),
+        '120' => new lang_string('numdays', '', 120),
+        '150' => new lang_string('numdays', '', 150),
+        '180' => new lang_string('numdays', '', 180),
+        '365' => new lang_string('numdays', '', 365),
+    );
+
+    $settings->add(new admin_setting_configselect('auth_email/expirationtime',
+        new lang_string('passwdexpiretime', 'auth_email'),
+        new lang_string('passwdexpiretime_desc', 'auth_email'), 180, $expirationtimeoptions));
+
+    $expirationwarningoptions = array(
+        '0' => new lang_string('never'),
+        '1' => new lang_string('numdays', '', 1),
+        '2' => new lang_string('numdays', '', 2),
+        '3' => new lang_string('numdays', '', 3),
+        '4' => new lang_string('numdays', '', 4),
+        '5' => new lang_string('numdays', '', 5),
+        '6' => new lang_string('numdays', '', 6),
+        '7' => new lang_string('numdays', '', 7),
+        '10' => new lang_string('numdays', '', 10),
+        '14' => new lang_string('numdays', '', 14),
+    );
+
+    $settings->add(new admin_setting_configselect('auth_email/expiration_warning',
+        new lang_string('expiration_warning', 'auth_email'),
+        new lang_string('expiration_warning_desc', 'auth_email'), 0, $expirationwarningoptions));
+
     // Display locking / mapping of profile fields.
     $authplugin = get_auth_plugin('email');
     display_auth_lock_options($settings, $authplugin->authtype, $authplugin->userfields,
